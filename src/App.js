f3c629bc015ba726a6507b83bc23bbaa5eec7f6d
bilalcelebi <bilalcelebi23@gmail.com>
2024-08-10 04:40:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const sources = ["Freedium", "Read Medium"];
+  const [inputValue, setInputValue] = useState('');
+  const [source, setSource] = useState(sources[0]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleClick = () => {
+    if (source === "Freedium") {
+      window.location.href = "https://www.freedium.cfd/" + inputValue
+    }
+    else if (source === "Read Medium") {
+      window.location.href = "https://readmedium.com/" + inputValue
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex items-center justify-center flex-col h-screen space-y-10 bg-gray-50">
+      <label className='text-center text-lg font-bold'>
+        Read Medium Free
+      </label>
+      <div className='flex items-center justify-center space-x-4'>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={inputValue}
+          onChange={handleInputChange}
+          className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+
+        <select
+        onChange={(e) => setSource(e.target.value)}
+        defaultValue={source}>
+          {
+            sources.map((source, idx) => (
+              <option key={idx} value={source}>{source}</option>
+            ))
+          }
+        </select>
+
+
+        <button
+          onClick={handleClick}
+          className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
